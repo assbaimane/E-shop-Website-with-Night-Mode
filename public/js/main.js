6c9e7131b2db05293e78ea747c7fcf656e388d70
Imane <assbaimane@hotmail.com>
2022-01-22 02:22:32 +0100

    JS - NightMode functionality added - V1 / Colors need improvement

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,31 @@
 // NavBar change color management
-
 let navBar = document.querySelector("nav");
 let logoBar = document.querySelector(".navLogo img");
-let section2Start = document.querySelector("#About");
+let aboutSection = document.querySelector("#About");
 
 window.addEventListener("scroll",()=>{
-    if(section2Start.getBoundingClientRect().top == 0){
+    if(aboutSection.getBoundingClientRect().top <= 0){
         navBar.classList.remove("bg-transparent","navbar-light");
         navBar.classList.add("bg-dark","navbar-dark","text-light");
-        logoBar.style.filter = "invert(1)"
+        logoBar.style.filter ="invert(1)"
     }else{}
+})
+
+// Night mode
+let nightMode = document.querySelector(".fa-moon");
+let homeSection = document.querySelector("#Home");
+let sectionAll = document.querySelectorAll("section");
+let beigeSection = document.querySelectorAll(".beige")
+
+nightMode.addEventListener("click",()=>{
+    homeSection.classList.add("night"); //adding a filter on home background-image
+    homeSection.style.backgroundImage = "url('./public/img/collect/one-page-slider-dark.png')";
+    document.querySelector("body").classList.add("night");
+
+    sectionAll.forEach(element => {
+        element.style.backgroundColor = "black"
+    });
+    beigeSection.forEach(element => {
+        element.style.backgroundColor = "#1e1e1e"
+    });
 })
